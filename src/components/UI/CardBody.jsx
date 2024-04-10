5e0ff7d8dcf1d9d613ef911307d865be4839f7a8
Andrew Biron <andrewbiron@andrews-air.home>
2024-04-09 20:26:41 -0400

    cards are created and layed out

--- src/components/UI/CardBody.jsx
+++ src/components/UI/CardBody.jsx
@@ -4,17 +4,17 @@
 export default function CardBody({title, image, repo, deployedApp}) {
   console.log(title, image, repo, deployedApp)
     return (
-      <>
-      <div className="card-body">
-        <h1>{title}</h1>
-        <img src={image} />
-        <p></p>
-        <a>{repo}</a><br></br>
-        <a>{deployedApp}</a>
-
+      <div className="card" style={{width: '20rem'}}>
+          <img src={image} className="card-img-top" alt="" />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card&apos;s content.</p>
+          </div>
+         
+          <div className="card-body">
+            <a href="#" className="card-link">{repo}</a>
+            <a href="#" className="card-link">{deployedApp}</a>
+          </div>
       </div>
-
-
-      </>
     );
   }
--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -7,42 +7,44 @@ export default function Portfolio() {
         <p>
             All about the work!
         </p>
+        <div className="card-container">
+          <CardBody 
+            title={"Music eShop"} 
+            image={'/public/images/musicEshop.png'} 
+            repo={"View Repository"} 
+            deployedApp={"View App"}/>
 
-        <CardBody 
-          title={"Music eShop"} 
-          image={'/public/images/iconLogo.png'} 
-          repo={"View Repository"} 
-          deployedApp={"View App"}/>
+          <CardBody 
+            title={"Global Glimpse Voyage"} 
+            image={'/public/images/unsplashImageChange.png'} 
+            repo={"View Repository"} 
+            deployedApp={"View App"}/>
 
-        <CardBody 
-          title={"Global Glimpse Voyage"} 
-          image={'/public/images/iconLogo.png'} 
-          repo={"View Repository"} 
-          deployedApp={"View App"}/>
+          <CardBody 
+            title={"Full-Stack Blog"} 
+            image={'/public/images/fullStack_1.png'} 
+            repo={"View Repository"} 
+            deployedApp={"View App"}/>
+        
+          <CardBody 
+            title={"JATE"} 
+            image={'/public/images/Jate.png'} 
+            repo={"View Repository"} 
+            deployedApp={"View App"}/>
 
-        <CardBody 
-          title={"Full-Stack Blog"} 
-          image={'/public/images/iconLogo.png'} 
-          repo={"View Repository"} 
-          deployedApp={"View App"}/>
-       
-       <CardBody 
-          title={"JATE"} 
-          image={'/public/images/iconLogo.png'} 
-          repo={"View Repository"} 
-          deployedApp={"View App"}/>
+          <CardBody 
+            title={"Note Taker"} 
+            image={'/public/images/note_taker_splash.png'} 
+            repo={"View Repository"} 
+            deployedApp={"View App"}/>
 
-        <CardBody 
-          title={"Note Taker"} 
-          image={'/public/images/iconLogo.png'} 
-          repo={"View Repository"} 
-          deployedApp={"View App"}/>
+          <CardBody 
+            title={"Workday Scheduler"} 
+            image={'/public/images/Work-Day-Scheduler.png'} 
+            repo={"View Repository"} 
+            deployedApp={"View App"}/>
 
-        <CardBody 
-          title={"Workday Scheduler"} 
-          image={'/public/images/iconLogo.png'} 
-          repo={"View Repository"} 
-          deployedApp={"View App"}/>
+         </div>
 
       </div>
     );
